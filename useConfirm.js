adbd393546edc33028964d7fde796562e8229c10
남이찬 <dwbh7358@sbs.co.kr>
2023-01-19 14:11:06 +0900

    feat: add useConfirm

---
+++ useConfirm.js
@@ -0,0 +1,18 @@
+export const useConfirm = (message = "", onConfirm, onCancle) => {
+  if (typeof onConfirm !== "function") {
+    return;
+  }
+  if (typeof onCancle !== "function") {
+    return;
+  }
+
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      onConfirm();
+    } else {
+      onCancle();
+    }
+  }
+
+  return confirmAction;
+}
